e5d4942a2f99a88321ca8c4f3995465403f7aa57
mattcbodily <mattcbodily@gmail.com>
2024-05-25 11:04:43 -0600

    Sets up reset.css, beginning global styles, and adds the sampleQuizData file

---
+++ sampleQuizData.js
@@ -0,0 +1,59 @@
+export const sampleQuizData = [
+  {
+    id: 1,
+    title: "Sample Quiz",
+    description: "This quiz provides sample quiz data for development purposes",
+    questions: [
+      {
+        answers: [
+          { correct: false, text: "Sample Answer #1" },
+          { correct: false, text: "Sample Answer #2" },
+          { correct: true, text: "Sample Answer #3" },
+          { correct: false, text: "Sample Answer #4" },
+        ],
+        text: "Sample Question #1",
+        type: "multiple-choice-select-one",
+      },
+      {
+        answers: [
+          { correct: false, text: "Sample Answer #1" },
+          { correct: false, text: "Sample Answer #2" },
+          { correct: true, text: "Sample Answer #3" },
+          { correct: false, text: "Sample Answer #4" },
+        ],
+        text: "Sample Question #2",
+        type: "multiple-choice-select-multiple",
+      },
+      {
+        answers: [
+          { correct: false, text: "Sample Answer #1" },
+          { correct: false, text: "Sample Answer #2" },
+          { correct: true, text: "Sample Answer #3" },
+          { correct: false, text: "Sample Answer #4" },
+        ],
+        text: "Sample Question #3",
+        type: "multiple-choice-select-one",
+      },
+      {
+        answers: [
+          { correct: false, text: "Sample Answer #1" },
+          { correct: false, text: "Sample Answer #2" },
+          { correct: true, text: "Sample Answer #3" },
+          { correct: false, text: "Sample Answer #4" },
+        ],
+        text: "Sample Question #4",
+        type: "multiple-choice-select-one",
+      },
+      {
+        answers: [
+          { correct: false, text: "Sample Answer #1" },
+          { correct: false, text: "Sample Answer #2" },
+          { correct: true, text: "Sample Answer #3" },
+          { correct: false, text: "Sample Answer #4" },
+        ],
+        text: "Sample Question #5",
+        type: "multiple-choice-select-multiple",
+      }
+    ]
+  }
+]
